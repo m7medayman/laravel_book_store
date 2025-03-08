0bd6f83a56aafb53dfd67a88166f2d1dfd50bcac
m7medayman <m7medmahgoop@gmail.com>
2025-03-08 23:28:52 +0200

    compelte itegration with breeze package

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
-use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/books',[BookController::class, 'index'])->name('books.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/books',[BookController::class, 'index'])->name('books.index');
 Route::get('/books/create',action: [BookController::class, 'create'])->name('books.create');
 Route::post('/books/store',action: [BookController::class, 'store'])->name('books.store');
 
@@ -14,4 +24,6 @@ Route::get('/books/{book}/edit',[BookController::class, 'edit'])->name('books.ed
 Route::get('/books/{book}',[BookController::class, 'show'])->name('books.show');
 Route::put('/books/{book}' ,[BookController::class, 'update'])->name('books.update');
 Route::delete('/books/{book}',[BookController::class, 'destroy'])->name('books.destroy');
+});
 
+require __DIR__.'/auth.php';
